cfb3be2d821769ecb04f9d4532541a241fbe9d37
YSNascimento <yuri.nascimento.ro@gmail.com>
2020-10-01 18:53:28 -0300

    header ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { QuoteGenerator } from './features/QuoteGenerator/QuoteGenerator';
+import { Header } from './features/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <QuoteGenerator />
       </header>
     </div>
