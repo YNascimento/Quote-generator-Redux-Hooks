cb010b0a2a5c5b29a637061a6d293541b7a56f38
YSNascimento <yuri.nascimento.ro@gmail.com>
2020-10-01 18:29:45 -0300

    troca telas ok

--- src/features/QuoteGenerator/QuoteGenerator.js
+++ src/features/QuoteGenerator/QuoteGenerator.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {setQuote, selectQuotes, selectAuthors} from './quoteGeneratorSlice'
-import styles from './Counter.module.css';
+import styles from './QuoteGenerator.module.css';
 
 function changeDisplay(){
     const showQuote = document.getElementById('showQuote')
     const addQuote = document.getElementById('addQuote')
 
-    if(showQuote.classList.contains('hide')){
-        showQuote.classList.remove('hide')
-        addQuote.classList.add('hide')
+    for (let i = 0; i < showQuote.classList.length; i++) {
+        if(showQuote.classList[i].includes('hide')){
+            addQuote.className += `, ${styles.hide}`
+            showQuote.className = ` ${styles.box}`
+            return
+        }
     }
-    else if(addQuote.classList.contains('hide')){
-        addQuote.classList.remove('hide')
-        showQuote.classList.add('hide')
+
+    for (let i = 0; i < addQuote.classList.length; i++) {
+        if(addQuote.classList[i].includes('hide')){
+            showQuote.className += ` ${styles.hide}`
+            addQuote.className = ` ${styles.box}`
+            return
+        }
     }
 }
 
@@ -31,78 +38,55 @@ export function QuoteGenerator() {
 
     let loadRandomQuote = () => {
         let n = Math.floor(Math.random()* allQuotes.length)
-        console.log(n)
-
         setPresentQuote(allQuotes[n])
         setPresentAuthor(allAuthors[n])
-
-        console.log(presentQuote)
     }
 
     return (
         <div>
-            <div>
-                <div id="showQuote" className={styles.box}>
-                    <h3>Click on "New Quote"</h3>
-                    <div id={styles.quote}>
-                        <p id="text">"{presentQuote}"</p>
-                        <small id="author">{presentAuthor}</small>  
-                    </div>
-                    
-                    <div className={styles.buttons}>
-                        <button className={styles.button} id="new-quote" onClick={loadRandomQuote}>Change Quote</button>
-
-                        <button className={styles.button}>Insert New Quote</button>
-
-                        <a href="/twitter.com/intent/tweet" className={styles.button} id="tweet-quote" target="_blank">
-                        Tweet Quote
-                        </a>
-                    </div>
+            <div id="showQuote" className={styles.box}>
+                <h3>Click on "New Quote"</h3>
+                <div id={styles.quote}>
+                    <p id="text">"{presentQuote}"</p>
+                    <small id="author">{presentAuthor}</small>  
                 </div>
+                
+                <div className={styles.buttons}>
+                    <button className={styles.button} id="new-quote" onClick={loadRandomQuote}>Change Quote</button>
 
-                <div id="addQuote" className={styles.box}>
-                    <div>
-                        <h3>Insert New Quote</h3>
-                        <form>
-                            <div id={styles.quote}>
-                                <input type='text' placeholder="Insert the Quote" name="quote" className={styles.input} value={newQuote} 
-                                    onChange={e => setNewQuote(e.target.value)}/>
-                                    
-                                <input type='text' placeholder="Insert the Author" name="author" className={styles.input} value={newAuthor} 
-                                    onChange={e => setNewAuthor(e.target.value)}/>
-                            </div>
-                            
-                            <div className={styles.buttons}>
-                                 <button className={styles.button} 
-                                    onClick={(e) => {e.preventDefault(); dispatch(setQuote( {newQuote, newAuthor}))}}>
-                                    Insert
-                                </button> 
-                                <button className={styles.button}>Back</button>
-                            </div>
-                        </form>
-                    </div>
+                    <button className={styles.button} onClick={changeDisplay}>Insert New Quote</button>
+
+                    <a href="/twitter.com/intent/tweet" className={styles.button} id="tweet-quote" target="_blank">
+                    Tweet Quote
+                    </a>
                 </div>
             </div>
 
-            {/* <div>
-                <div className={styles.row}>
-                    <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}> + </button>
+            <div id="addQuote" className={`${styles.box}, ${styles.hide}`}>
+                <div>
+                    <h3>Insert New Quote</h3>
+                    <form>
+                        <div id={styles.quote}>
+                            <input type='text' placeholder="Insert the Quote" name="quote" className={styles.input} value={newQuote} 
+                                onChange={e => setNewQuote(e.target.value)}/>
 
-                    <span className={styles.value}>{count}</span>
-
-                    <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}> - </button>
+                            <input type='text' placeholder="Insert the Author" name="author" className={styles.input} value={newAuthor} 
+                                onChange={e => setNewAuthor(e.target.value)}/>
+                        </div>
+                        
+                        <div className={styles.buttons}>
+                            <button className={styles.button} 
+                                onClick={(e) => {e.preventDefault(); dispatch(setQuote( {newQuote, newAuthor}))}}>
+                                Insert
+                            </button>
+                        </div>
+                    </form>
+                    
+                    <div className={styles.buttons}>
+                        <button className={styles.button} onClick={changeDisplay}>Back</button>
+                    </div>
                 </div>
-
-                <div className={styles.row}>
-                    <input className={styles.textbox} aria-label="Set increment amount" value={incrementAmount} onChange={e => setIncrementAmount(e.target.value)} />
-                    <button className={styles.button} onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}>
-                        Add Amount
-                    </button>
-                    <button className={styles.asyncButton} onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
-                        Add Async
-                    </button>
-                </div>
-            </div> */}
+            </div>
         </div>
     );
 }
